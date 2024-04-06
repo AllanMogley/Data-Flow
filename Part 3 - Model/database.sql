ce8643c1185250d1565aed8549b8bfe69a8ee5e8
allanmogley <allanmogley@gmail.com>
2024-04-06 19:59:02 +0300

    Data Import Success

--- Part 3 - Model/database.sql
+++ Part 3 - Model/database.sql
@@ -2,14 +2,14 @@ create database tredstone;
 create schema if not exists flattable;
 alter database tredstone set search_path to flattable;
 
-create table if not exists 'raw_data'
+create table if not exists raw_data
 	(
-	Date : date,
-        Open : numeric,
-        High : numeric,
-        Low : numeric,
-        Close : numeric,
-        AdjClose : numeric,
-        Volume : numeric,
-        company_name : varchar(20)
+	date date,
+        open numeric,
+        high numeric,
+        low numeric,
+        close numeric,
+        adjClose numeric,
+        volume numeric,
+        company_name varchar(20)
 	)
