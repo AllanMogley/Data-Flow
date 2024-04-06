44fedddbb33256f4b8de98e3bf5538a1335a731a
allanmogley <allanmogley@gmail.com>
2024-04-06 19:54:00 +0300

    staged files 2

--- Part 3 - Model/database.sql
+++ Part 3 - Model/database.sql
@@ -1,3 +1,15 @@
 create database tredstone;
 create schema if not exists flattable;
 alter database tredstone set search_path to flattable;
+
+create table if not exists 'raw_data'
+	(
+	Date : date,
+        Open : numeric,
+        High : numeric,
+        Low : numeric,
+        Close : numeric,
+        AdjClose : numeric,
+        Volume : numeric,
+        company_name : varchar(20)
+	)
